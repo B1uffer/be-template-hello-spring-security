b6209436c3dc7e0aed1c4449f63f7f31f5a6a895
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-29 12:09:59 +0900

    FilterChain 메서드 생성

--- src/main/java/com/springboot/config/SecurityConfiguration.java
+++ src/main/java/com/springboot/config/SecurityConfiguration.java
@@ -1,13 +1,24 @@
 package com.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+    // FilterChain 구성하기
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // HttpSecurity를 통해 HTTP 요청에 대한 보안 설정 구성하기
+        
+        return http.build();
+    }
+
     // InMemory User로 인증하기
     public UserDetailsManager userDetailsManager() {
         UserDetails userDetails = User.withDefaultPasswordEncoder() // 더이상 사용되지 않음
