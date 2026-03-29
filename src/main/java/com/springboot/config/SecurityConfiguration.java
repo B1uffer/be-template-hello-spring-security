7b3b89c7b142702e1a03ae08cc67e7cc8c47deed
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-29 12:06:40 +0900

    UserDetailsManaer 타입의 userDetailsManager() 메서드 생성 InMemory에 user를 생성하여 user등급의 인증을 하게 해줌 withDefaultPasswordEncoder() 메서드는 더이상 사용되지 않는다

--- src/main/java/com/springboot/config/SecurityConfiguration.java
+++ src/main/java/com/springboot/config/SecurityConfiguration.java
@@ -1,7 +1,20 @@
 package com.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration {
+    // InMemory User로 인증하기
+    public UserDetailsManager userDetailsManager() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder() // 더이상 사용되지 않음
+                .username("user")
+                .password("1111")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 }
